429e542c61e971903ca37d78b4ce479a2a6db254
stzeng02 <158509447+stzeng02@users.noreply.github.com>
2024-04-22 19:20:47 -0500

    Ported kaggle example over

--- scripts/study_case/CIFAR_10/cifar_example.py
+++ scripts/study_case/CIFAR_10/cifar_example.py
@@ -1,6 +1,9 @@
 # pip install tensorflow==1.5.0
 # pip install keras==2.1.2
+# pip install matplotlib
 # https://www.kaggle.com/code/viratkothari/image-classification-of-cifar-10-using-tensorflow 
+
+# python -m scripts.study_case.CIFAR_10.cifar_example
 
 import tensorflow as tf
 from tensorflow import keras
@@ -40,3 +43,83 @@ ytest=to_categorical(ytest)
 # After one hot encoding
 print("ytrain Shape: %s and value: %s" % (ytrain.shape, ytrain[0]))
 print("ytest Shape: %s and value: %s" % (ytest.shape, ytest[1]))
+
+# Modelling - Model on CNN
+
+from keras import models, layers
+from keras.layers import Conv2D, MaxPooling2D, Flatten , Dense, Activation,Dropout
+
+# create a sequential model i.e. empty neural network which has no layers in it.
+model=models.Sequential()
+
+#==================== Feature Detection / extraction Block ====================#
+
+# Add first convolutional block - To deal with images we use Conv2D and for colour images and shape use Conv3D
+#model.add(layers.Conv2D(filters=6, kernal_size(3,3), input_shape=(32,32,1), activation='relu'))
+# in the first block we need to mention input_shape
+model.add(layers.Conv2D(64,(3,3),input_shape=(32,32,3),activation='relu'))
+model.add(layers.Conv2D(64,(3,3),input_shape=(32,32,3),activation='relu'))
+# Add the max pooling layer
+model.add(layers.MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+# Add Second convolutional block
+#model.add(layers.Conv2D(filters=6, kernal_size(3,3), activation='relu'))
+model.add(layers.Conv2D(128,(3,3),activation='relu'))
+model.add(layers.Conv2D(128,(3,3),activation='relu'))
+# Add the max pooling layer
+model.add(layers.MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+# Add Third convolutional block
+#model.add(layers.Conv2D(filters=6, kernal_size(3,3), activation='relu'))
+model.add(layers.Conv2D(256,(3,3),activation='relu'))
+# model.add(layers.Conv2D(256,(3,3),activation='relu'))
+# Add the max pooling layer
+model.add(layers.MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+#==================== Transition Block (from feature detection to classification) ====================#
+
+# Add Flatten layer. Flatten simply converts matrics to array
+model.add(layers.Flatten(input_shape=(32,32))) # this will flatten the image and after this Classification happens
+
+#==================== Classification Block ====================#
+
+# Classification segment - fully connected network
+# The Dence layer does classification and is deep neural network. Dense layer always accept the array.
+model.add(layers.Dense(128, activation='relu')) # as C5 layer in above image. 
+model.add(layers.Dense(100, activation='relu')) # as C5 layer in above image. 
+model.add(layers.Dense(80, activation='relu')) # as C5 layer in above image. 
+# model.add(layers.Dense(60, activation='relu')) # as C5 layer in above image
+# model.add(layers.Dense(40, activation='relu')) # as C5 layer in above image
+# this 120 is hyper parameter whcih is number of neuron 
+#model.add(layers.Dense(84, activation='relu'))# as F6 layer in aboave image
+
+# Add the output layer
+model.add(layers.Dense(10, activation='softmax')) # as Output layer in above image. The output layer normally have softmax activation
+
+# Compile the model
+
+# if we use softmax activation in output layer then best fit optimizer is categorical_crossentropy
+# for sigmoid activation in output layer then loss will be binary_crossentropy
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) 
+# if we do not go for One Hot Encoding then use loss='sparse_categorical_crossentropy'
+
+# Train the model 
+# Using GPU really speeds up this code
+xtrain2=xtrain.reshape(50000,32,32,3)
+xtest2=xtest.reshape(10000,32,32,3)
+
+print(xtrain.shape)
+print(xtest.shape)
+print(ytrain.shape)
+print(ytest.shape)
+
+# Training has been reduces to just 2 epochs due to lack of GPU support for Windows 11 in TF 1.5 (needs CUDA 9.0)
+model.fit(xtrain2,ytrain,epochs=2,batch_size=56,verbose=True,validation_data=(xtest2,ytest))
+
+# Testing the model
+test_loss, test_acc = model.evaluate(xtest2, ytest)
+print("accuracy:", test_acc)
